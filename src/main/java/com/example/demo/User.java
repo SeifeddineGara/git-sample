dc1b62de503176d64a12563d940b4dde793bc1cd
Seifeddine Gara <gara.seif@gmail.com>
2023-07-18 10:15:25 +0200

    commit test

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -2,16 +2,24 @@ package com.example.demo;
 
 public class User {
 
-    private String username ;
+    private String userName ;
+	private String lastName
     private String email ;
 
 
-    public String getUsername() {
-        return username;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+	public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
